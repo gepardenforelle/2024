528c4880c84a867172510db40eefa448a423eb96
MartinW <24792082+gepardenforelle@users.noreply.github.com>
2024-01-27 23:39:45 +0100

    python2

---
+++ python/guestlist.py
@@ -0,0 +1,6 @@
+guests = ['Z', 'A', 'B', 'C']
+for g in guests:
+    print("Hello ", g)
+    
+print(guests)
+print(sorted(guests))
---
+++ python/lists1.py
@@ -0,0 +1,6 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+print(bicycles[1])
+bicycles.append('fahrrad')
+bicycles.insert(0, 'bike')
+print(bicycles)
